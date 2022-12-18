6601b555c94df82a3d7890713d803fb870c5e4d9
Ryeno <ryeno@Rye-Welz-Geselowitz-C02C915RLVDF-SW.local>
2022-12-18 14:44:01 -0500

    clarify gameplay / rules

--- rules.js
+++ rules.js
@@ -1,10 +1,27 @@
 const RULES = [
-    'Players take turns acting as the Reader',
-    'The Reader generates a prompt and reads aloud the case title and "introduction quote."',
-    'Other players write a plausible "conclusion quote" for the case',
-    'The Reader writes the actual "conclusion quote."',
-    'Players hand in their entries.',
-    'The Reader shuffles all entries, including the actual quote, and reads them aloud',
-    '<b>Players guess which quote comes from the actual case.</b>',
-    'Players win glory for (1) guessing the real quote and (2) having their fake quote guessed.'
+    {
+      'header': 'Rules',
+      'items': [
+        'Players take turns acting as the Reader.',
+        'The Reader clicks "Next Prompt" and reads aloud the case title and "introduction quote."',
+        'Each other player writes a plausible "conclusion quote" for the case.',
+        'The Reader writes the actual "conclusion quote."',
+        'Players hand in their entries to the Reader.',
+        'The Reader shuffles all entries, including the actual quote, and reads them aloud.',
+        '<b>Players guess which quote comes from the actual court case.</b>',
+      ]
+    },
+    {
+      'header': 'Scoring',
+      'items': [
+        '+1 point for every vote your entry earned',
+        '+2 points for guessing the correct answer'
+      ]
+    },
+    {
+      'header': 'Materials',
+      'items': [
+        'Each player needs: this app, paper, pencil.',
+      ]
+    },
 ]
