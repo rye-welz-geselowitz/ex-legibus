2c897ab1d5b958f6f3442e38495574cda5abd6c7
Ryeno <ryeno@Rye-Welz-Geselowitz-C02C915RLVDF-SW.local>
2022-12-18 14:10:39 -0500

    add more rrounds; fix broken curly quotes; display court name and year

--- rounds_selection.js
+++ rounds_selection.js
@@ -1,5 +1,18 @@
-
 const PARSED_ROUNDS = $.csv.toObjects(ROUNDS_CSV);
+
+PARSED_ROUNDS.forEach((round)=>{
+  Object.keys(round).forEach((key)=>{
+    round[key] = reformat(round[key])
+  })
+})
+
+function reformat(string){
+  return string.replace('“', '"')
+    .replace('”', '"')
+    .replace("’", "'")
+    .replace("‘", "'")
+}
+
 
 function getRandomItem(items){
   return items[Math.floor(Math.random()*items.length)];
