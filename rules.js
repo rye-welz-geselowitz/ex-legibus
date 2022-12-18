6310a70f54ab9d6605302b2efb0602c016ddb8d9
Ryeno <ryeno@Ryeno-C02C915RLVDF-SW.local>
2022-12-17 21:05:46 -0500

    add styling and rules

---
+++ rules.js
@@ -0,0 +1,10 @@
+const RULES = [
+    'Players take turns acting as the Reader',
+    'The Reader generates a prompt and reads aloud the case title and "introduction quote."',
+    'Other players write a plausible "conclusion quote" for the case',
+    'The Reader writes the actual "conclusion quote."',
+    'Players hand in their entries.',
+    'The Reader shuffles all entries, including the actual quote, and reads them aloud',
+    '<b>Players guess which quote comes from the actual case.</b>',
+    'Players win glory for (1) guessing the real quote and (2) having their fake quote guessed.'
+]
